80d4f063e6b81eceea9f0184e1025ef82377c907
Nick Hollon <falling.masonry@gmail.com>
2015-06-21 16:59:52 -0700

    Draw an equator

--- Main.elm
+++ Main.elm
@@ -1,3 +1,20 @@
-import Graphics.Element
+import Graphics.Element as Element
+import Graphics.Collage as Graphics
+import Array as A
+import Color
 
-main = Graphics.Element.show "Hello, Globe!"
+main = Graphics.collage 360 180 (mapForms)
+  |> Element.color Color.white
+  |> Element.container 600 200 Element.middle
+  |> Element.color Color.black
+  |> Element.container 620 220 Element.middle
+
+mapForms : List Graphics.Form
+mapForms = A.initialize 24 (\i -> plot (toFloat i, 0.0)) |> A.toList
+
+plot : (Float, Float) -> Graphics.Form
+plot (ra, dec) = 
+  let
+    x = ra * 15 - 180
+    y = dec
+  in Graphics.circle 5 |> Graphics.filled Color.blue |> Graphics.move (x, y)
