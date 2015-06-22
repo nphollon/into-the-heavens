7d7a46132ba2b159ced5f0aaf9e90ed17ce4d99f
Nick Hollon <falling.masonry@gmail.com>
2015-06-21 18:15:19 -0700

    Reorganize top-level functions

--- Main.elm
+++ Main.elm
@@ -4,29 +4,30 @@ import Array as A
 import List as L
 import Color
 
-main =
-  let 
-    width = 600
-    height = 400
-    xBox = 900
-    yBox = 450
-    padding = 20
-  in Graphics.collage width height (cartography (width, height))
-    |> Element.color Color.white
-    |> Element.container xBox yBox Element.middle
-    |> Element.color Color.black
-    |> Element.container (xBox + padding) (yBox + padding) Element.middle
-
-cartography : (Float, Float) -> List Graphics.Form
-cartography bounds = let
-  toForm (curve, color) = L.map (fromRaDec bounds) curve |> Graphics.path |> Graphics.traced (Graphics.solid color)
-  in L.map toForm [
+main = canvas <| cartography (600, 400) <| [
     (parallel 0, Color.blue),
     (parallel 23.5, Color.red),
     (parallel -23.5, Color.red),
     (meridian 6, Color.blue),
     (meridian 18, Color.red)
   ]
+
+canvas : List Graphics.Form -> Element.Element
+canvas forms = 
+  let 
+    width = 600
+    height = 400
+    xBox = 900
+    yBox = 450
+    padding = 20
+  in Graphics.collage width height forms
+    |> Element.color Color.white
+    |> Element.container xBox yBox Element.middle
+    |> Element.color Color.black
+    |> Element.container (xBox + padding) (yBox + padding) Element.middle
+
+cartography : (Float, Float) -> List (List (Float, Float), Color.Color) -> List Graphics.Form
+cartography bounds = L.map (\(curve, color) -> L.map (fromRaDec bounds) curve |> Graphics.path |> Graphics.traced (Graphics.solid color))
 
 parallel : Float -> List (Float, Float)
 parallel declination = A.initialize 24 (\i -> (toFloat i, declination)) |> A.toList
