270b4d5a768be3f41561184c9b38ba1d660266c4
Nick Hollon <falling.masonry@gmail.com>
2015-06-21 17:47:55 -0700

    Add tropics in red

--- Main.elm
+++ Main.elm
@@ -4,26 +4,21 @@ import Array as A
 import List as L
 import Color
 
-main = Graphics.collage 360 180 (mapForms)
+main = Graphics.collage 360 180 (cartography)
   |> Element.color Color.white
   |> Element.container 600 200 Element.middle
   |> Element.color Color.black
   |> Element.container 620 220 Element.middle
 
-mapForms : List Graphics.Form
-mapForms = [
-  L.map fromRaDec equator |> plotCurve Color.blue,
-  L.map fromRaDec primeMeridian |> plotCurve Color.red
-  ]
+cartography : List Graphics.Form
+cartography = let
+  toForm (curve, color) = L.map fromRaDec curve |> plotCurve color
+  in L.map toForm [ (equator, Color.blue), (primeMeridian, Color.blue), (cancer, Color.red), (capricorn, Color.red) ]
 
-equator : List (Float, Float)
-equator = 
-  let
-    toRaDec i = (toFloat i, 0.0)
-  in A.initialize 24 toRaDec |> A.toList
-
-primeMeridian : List (Float, Float)
+equator = A.initialize 24 (\i -> (toFloat i, 0.0)) |> A.toList
 primeMeridian = A.initialize 19 (\i -> (12, 10 * toFloat i - 90)) |> A.toList
+cancer = A.initialize 24 (\i -> (toFloat i, 23.5)) |> A.toList
+capricorn = A.initialize 24 (\i -> (toFloat i, -23.5)) |> A.toList
 
 fromRaDec (ra, dec) = (ra * 15 - 180, dec)
 
