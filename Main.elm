a29dd7835ee918a6b161d72899ba5e34a28bb891
Nick Hollon <falling.masonry@gmail.com>
2015-06-27 13:50:16 -0700

    Implement image tiling. Refactor rendering code into a pipeline. Make the world spin!

--- Main.elm
+++ Main.elm
@@ -1,59 +1,105 @@
-import Graphics.Element as Element
-import Graphics.Collage as Graphics
+import Graphics.Element as Element exposing (Element)
+import Graphics.Collage as Graphics exposing (Form)
 import Array as A
+import Dict as D
 import List as L
-import Color
+import Time
+import Signal exposing ((<~))
+import Color exposing (Color)
 
-type alias Dimensions = (Float, Float)
-type WorldPoint = WPt (Float, Float)
-type CameraPoint = CPt (Float, Float)
+{-
+We have a list of world elements
+
+Transform all world coordinates to the reference frame of the camera
+This is the transformed world
+
+Boundaries of the camera
+Tile the transformed world elements to cover the expanse of the camera
+Clip the world elements that are outside the boundaries of the camera
+This is the visible world
+
+Boundaries of the canvas
+transform the camera coordinates to screen coordinates
+  -}
+
+type alias CameraPosition = (Float, Float)
+type alias CameraDimensions = (Float, Float)
+type alias Dimensions = (Int, Int)
+type alias WorldPoint = (Float, Float)
 type alias ScreenPoint = (Float, Float)
 
-main = canvas <| cartography (600, 400) -4 <| [
+type alias Entity = (List WorldPoint, Color)
+type alias Image = (List ScreenPoint, Color)
+
+tau = 2 * pi
+
+entities = [
     (parallel 0, Color.blue),
     (parallel 23.5, Color.red),
     (parallel -23.5, Color.red),
     (meridian 0, Color.blue),
-    (meridian 12, Color.red)
+    (meridian 12, Color.red),
+    ([(1, 15), (7, 50)], Color.black)
   ]
 
-canvas : List Graphics.Form -> Element.Element
-canvas forms = 
-  let 
-    width = 600
-    height = 400
-    xBox = 900
-    yBox = 450
-    padding = 20
-  in Graphics.collage width height forms
-    |> Element.color Color.white
-    |> Element.container xBox yBox Element.middle
-    |> Element.color Color.black
-    |> Element.container (xBox + padding) (yBox + padding) Element.middle
+main = graphics << (*) 0.01 <~ Time.every (Time.millisecond * 10)
 
-cartography : Dimensions -> Float -> List (List WorldPoint, Color.Color) -> List Graphics.Form
-cartography bounds azimuth = 
+graphics : Float -> Element
+graphics cRa = project (cRa, 0) entities
+  |> tile (degrees 360, degrees 180)
+  |> plot (600, 400)
+  |> render (600, 400)
+  |> frame (900, 450)
+
+frame : Dimensions -> Element -> Element
+frame (width, height) = 
+  let padding = 20
+  in Element.container width height Element.middle
+    >> Element.color Color.black
+    >> Element.container (width + padding) (height + padding) Element.middle
+
+render : Dimensions -> List Form -> Element
+render (width, height) = Graphics.collage width height >> Element.color Color.white
+
+plot : CameraDimensions -> List Image -> List Form
+plot dim = L.map (scale dim >> toForm)
+
+toForm : Image -> Form
+toForm (pts, color) = Graphics.traced (Graphics.solid color) pts
+
+scale : CameraDimensions -> Image -> Image
+scale dim (pts, color) = (L.map (toScreen dim) pts, color)
+
+toScreen : CameraDimensions -> ScreenPoint -> ScreenPoint
+toScreen (width, height) (lon, lat) =
   let
-    transform = toCamera azimuth >> toScreen bounds
-    drawCurve (curve, color) = L.map transform curve |> Graphics.path |> Graphics.traced (Graphics.solid color)
-  in L.map drawCurve
+    x = lon * width
+    y = lat * height * 2
+  in (x, y)
+
+project : CameraPosition -> List Entity -> List Image
+project center = L.map (\(pts, color) -> (L.map (toCamera center) pts, color))
+
+toCamera : CameraPosition -> WorldPoint -> ScreenPoint
+toCamera (cRa, cDec) (pRa, pDec) = 
+  let
+    lon = (pRa - cRa) / 24
+    lat = (pDec - cDec) / 360
+  in (lon, lat)
+
+tile : CameraDimensions -> List Image -> List Image
+tile _ = L.concatMap tileImage
+
+tileImage : Image -> List Image
+tileImage (pts, color) =
+  let
+    addIfEmpty key f dict = if D.member key dict then dict else D.insert key (f key) dict
+    offset dx = L.map (\(x, y) -> (x - dx, y))
+    addImage (x,_) = addIfEmpty (round x) (\i -> (offset (toFloat i) pts, color))
+  in L.foldl addImage D.empty pts |> D.values
 
 parallel : Float -> List WorldPoint
-parallel declination = A.initialize 12 (\i -> WPt (toFloat i, declination)) |> A.toList
+parallel declination = A.initialize 25 (\i -> (toFloat i, declination)) |> A.toList
 
 meridian : Float -> List WorldPoint
-meridian rightAscension = A.initialize 19 (\i -> WPt (rightAscension, 10 * toFloat i - 90)) |> A.toList
-
-toCamera : Float -> WorldPoint -> CameraPoint
-toCamera azimuth (WPt (ra, dec)) = 
-  let
-    lon = degrees <| (ra - azimuth) * 15
-    lat = degrees dec
-  in CPt (lon, lat)
-
-toScreen : Dimensions -> CameraPoint -> ScreenPoint
-toScreen (width, height) (CPt (lon, lat)) =
-  let
-    x = (lon / pi - 1) * 0.5 * width
-    y = lat / pi * height
-  in (x, y)
+meridian rightAscension = A.initialize 19 (\i -> (rightAscension, 10 * toFloat i - 90)) |> A.toList
