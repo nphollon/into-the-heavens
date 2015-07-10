d28bf9ed5e786be4ebe3adf9f44a74576ebfcd91
Nick Hollon <falling.masonry@gmail.com>
2015-07-10 12:59:32 -0700

    Add gradient to triangle and make it spin. Document open questions about elm-webgl

--- src/WebGLDemo.elm
+++ src/WebGLDemo.elm
@@ -1,47 +1,108 @@
 module WebGLDemo where
 
 import WebGL
-import Math.Vector2 as V2
+import Math.Vector3 as V3
+import Math.Matrix4 as M4
 import Graphics.Element as Layout
 import Color
+import Signal
+import Time
 
-main : Layout.Element
+{-
+  optimization questions:
+    triangle fans & strips
+    rendering faces in one direction only
+    referencing vertices by index
+  -}
+
+main : Signal Layout.Element
 main =
+  let
+    startState =
+      0.0
+
+    model =
+      Signal.foldp update startState signal
+  in
+    Signal.map view model
+
+
+type alias Model =
+  Float
+
+
+type Action =
+  Rotate
+
+
+signal : Signal Action
+signal =
+  Signal.map (always Rotate) (Time.every (50 * Time.millisecond))
+
+
+update : Action -> Model -> Model
+update action yaw =
+  case action of
+    Rotate ->
+      yaw + degrees 5
+
+
+view : Model -> Layout.Element
+view yaw =
   let
     x = 300
     y = 200
-  in WebGL.webgl (x, y) [ tringle ]
+  in WebGL.webgl (x, y) [ tringle yaw ]
     |> Layout.container x y Layout.middle
     |> Layout.color Color.black
 
-tringle : WebGL.Entity
-tringle =
+
+tringle : Model -> WebGL.Entity
+tringle yaw =
   let
     mesh =
       [
-        ({ position = V2.vec2 0 0.5 }
-        ,{ position = V2.vec2 0.5 0.5 }
-        ,{ position = V2.vec2 0.5 0 })
+        ({ position = V3.vec3 0 0.3 0 }
+        ,{ position = V3.vec3 0.5 0.3 0 }
+        ,{ position = V3.vec3 0.5 -0.3 0 })
       ]
-  in
-    WebGL.entity vertexShader fragmentShader mesh { }
 
-type alias Attribute = { position : V2.Vec2 }
-type alias Uniform = { }
-type alias Varying = { }
+    rotation = M4.makeRotate yaw (V3.vec3 0 1 0)
+    translation = M4.makeTranslate (V3.vec3 0 0 -1)
+
+    uniform =
+      { perspective = M4.makePerspective 90 1.5 0.1 10
+      , placement = M4.mul translation rotation
+      }
+  in
+    WebGL.entity vertexShader fragmentShader mesh uniform
+
+type alias Attribute = { position : V3.Vec3 }
+type alias Uniform = { perspective : M4.Mat4, placement : M4.Mat4 }
+type alias Varying = { rg : V3.Vec3 }
 
 vertexShader : WebGL.Shader Attribute Uniform Varying
-vertexShader = [glsl|
-  attribute vec2 position;
+vertexShader =
+  [glsl|
+  attribute vec3 position;
+  uniform mat4 perspective;
+  uniform mat4 placement;
+  varying vec3 rg;
 
   void main() {
-    gl_Position = vec4(position, 0, 1);
+    gl_Position =
+      perspective * (placement * vec4(position, 1.0));
+    rg = vec3(1.0, position.xy);
   }
   |]
 
 fragmentShader : WebGL.Shader { } Uniform Varying
-fragmentShader = [glsl|
+fragmentShader =
+  [glsl|
+  precision mediump float;
+  varying vec3 rg;
+
   void main () {
-    gl_FragColor = vec4(1, 0, 0, 1);
+    gl_FragColor = vec4(rg, 1.0);
   }
   |]
