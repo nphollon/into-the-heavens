9921c5ab9a5deb3eba596feece41fe13f7af061e
Nick Hollon <falling.masonry@gmail.com>
2015-07-19 22:49:02 -0700

    Replace Main with WebGLDemo

--- src/Main.elm
+++ src/Main.elm
@@ -1,147 +1,204 @@
 module Main where
 
+import WebGL
+import Math.Vector3 as Vec3
+import Math.Vector4 as Vec4
+import Math.Matrix4 as Mat4
 import Graphics.Element as Layout
-import Graphics.Collage as Graphics
-import Html
 import Color
 import Signal
 import Time
 import Keyboard
 import List
+import Array
 
-import Grid
-import Constellation
-import Scatter
+import Graphics
+import Infix exposing (..)
 
-type alias CameraPosition = 
-  Point
-
-type alias Dimensions = 
-  (Int, Int)
-
-type alias Point = 
-  (Float, Float)
-
-type alias Image = 
-  { points : List Point
-  , draw : List Point -> Graphics.Form
-  }
-
-type alias Action = 
-  { x : Int
-  , y : Int 
-  }
-
-type alias Model =
-  { camera : CameraPosition
-  , entities : List Image
-  }
-
-
-tau : Float
-tau =
-  turns 1
-
-
-main : Signal Html.Html
+main : Signal Layout.Element
 main =
   let
     startState =
-      { camera = (0, degrees 90)
-      , entities =
-        [ Grid.parallel 0
-        , Grid.parallel 23.5
-        , Grid.parallel -23.5
-        , Grid.parallel 66.5
-        , Grid.parallel -66.5
-        , Grid.meridian 0
-        , Grid.meridian 12
-        , Grid.meridian 6
-        , Grid.meridian 18
-        , Scatter.scatter 200
-        , Constellation.crux
-        , Constellation.ursaMajor
-        , Constellation.aquarius
-        ]
+      { orientation = Mat4.identity
       }
 
     model =
-      Signal.foldp update startState keyboardSignal
+      Signal.foldp update startState signal
   in
     Signal.map view model
 
 
-update : Action -> Model -> Model
-update action model = 
-  let
-    delta =
-      (degrees 5, degrees 5)
-
-    newRa =
-      toFloat action.x * fst delta + fst model.camera
-
-    newDec =
-      toFloat action.y * snd delta + snd model.camera
-  in
-    { model | camera <- (newRa, newDec) }
+type alias Model =
+  { orientation : Mat4.Mat4
+  }
 
 
-keyboardSignal : Signal Action
-keyboardSignal = 
+type alias Action =
+  { x : Int
+  , y : Int
+  }
+
+type alias Attribute =
+  { position : Vec3.Vec3
+  , vertColor : Vec4.Vec4
+  }
+
+
+type alias Uniform =
+  { perspective : Mat4.Mat4
+  , placement : Mat4.Mat4
+  }
+
+
+type alias Varying = {
+  fragColor : Vec4.Vec4
+  }
+
+
+signal : Signal Action
+signal =
   Signal.sampleOn (Time.every (10 * Time.millisecond)) Keyboard.wasd
 
 
-view : Model -> Html.Html
+update : Action -> Model -> Model
+update action model =
+  let
+    delta =
+      degrees 1
+
+    newOrientation =
+      model.orientation
+      |> Mat4.rotate (action.y .* delta) xAxis
+      |> Mat4.rotate (negate action.x .* delta) yAxis
+  in
+    { model | orientation <- newOrientation }
+
+
+view : Model -> Layout.Element
 view model =
   let
-    screenDims = 
-      (600, 400)
+    dimensions =
+      (600, 600)
 
-    frameDims =
-      (900, 450)
+    aspect =
+      uncurry (./.) dimensions
+
+    uniform =
+      { perspective = Mat4.makePerspective 90 aspect 1 70
+      , placement = Mat4.transpose model.orientation
+      }
   in
-    project model.camera model.entities
-    |> plot screenDims
-    |> uncurry Graphics.collage screenDims
-    |> Layout.color (Color.hsl (degrees 240) 1 0.1)
-    |> uncurry Layout.container frameDims Layout.middle
-    |> Layout.color Color.black
-    |> Html.fromElement
+    Graphics.render dimensions
+      [ compass uniform
+      , meridian 30 (degrees 45) uniform
+      , meridian 30 (degrees 135) uniform
+      ]
+      
 
-
-plot : Dimensions -> List Image -> List Graphics.Form
-plot dim =
-  List.map
-    (\image -> List.map (toScreen dim) image.points |> image.draw )
-
-
-toScreen : Dimensions -> Point -> Point
-toScreen (width, height) (lon, lat) =
+compass : Uniform -> WebGL.Entity
+compass uniform =
   let
-    x = 
-      lon * (toFloat width) / tau
-
-    y = 
-      lat * (toFloat height) * 2 / tau
+    mesh =
+      List.concat
+      [ star 25 0 0
+      , star 25 0 (degrees 180)
+      , star 25 0 (degrees 90)
+      , star 25 (degrees 90) (degrees 90)
+      , star 25 (degrees 180) (degrees 90)
+      , star 25 (degrees 270) (degrees 90)
+      ]
   in
-    (x, y)
+    Graphics.entity mesh uniform
 
 
-project : CameraPosition -> List Image -> List Image
-project center =
-  List.map (\image -> { image | points <- List.map (toCamera center) image.points } )
-
-
-toCamera : CameraPosition -> Point -> Point
-toCamera (lon, lat) (ra, dec) = 
+star : Float -> Float -> Float -> List (WebGL.Triangle Attribute)
+star r phi theta =
   let
-    hour = 
-      lon - ra
+    move =
+      Mat4.makeRotate phi yAxis
+      |> Mat4.rotate theta zAxis
+      |> Mat4.translate (Vec3.vec3 0 r 0)
+      |> Graphics.transform
 
-    alt = 
-      asin ((sin lat) * (sin dec) + (cos lat) * (cos dec) * (cos hour))
+    down = move <| Graphics.vertex Color.yellow 0 -1 0
+    up = move <| Graphics.vertex Color.white 0 1 0
+    east = move <| Graphics.vertex Color.yellow -1 0 0
+    west = move <| Graphics.vertex Color.white 1 0 0
+    south = move <| Graphics.vertex Color.yellow 0 0 -1
+    north = move <| Graphics.vertex Color.white 0 0 1
 
-    az =
-      atan2 (sin hour) ((cos hour) * (sin lat) - (tan dec) * (cos lat))
-  in 
-    (az, alt)
+  in
+    [ (down, north, up)
+    , (down, south, up)
+    , (down, east, up)
+    , (down, west, up)
+    , (east, north, west)
+    , (east, south, west)
+    ]
+
+
+meridian : Float -> Float -> Uniform -> WebGL.Entity
+meridian radius azimuth uniform = 
+  let
+    baseRing =
+      vertexRing 50 0.005
+
+    rotation =
+      Mat4.makeRotate azimuth yAxis
+      |> Mat4.rotate (degrees 90) zAxis
+
+    ring =
+      List.map (Graphics.transform rotation >> Graphics.scale radius) baseRing
+
+    mesh =
+      List.map3 (\a b c -> (a,b,c))
+        ring
+        (List.drop 1 ring)
+        (List.drop 2 ring)
+  in
+    Graphics.entity mesh uniform
+
+
+vertexRing : Int -> Float -> List Attribute
+vertexRing resolution width =
+  let
+    grate =
+      2 * resolution
+
+    indexedVertex i =
+      let
+        side =
+          if (i % 2 == 0) then 1 else -1
+
+        phi =
+          turns (i ./. grate)
+      in
+        sphVertex Color.red 1 phi (degrees 90 + side .* width)
+  in
+    Array.initialize (grate + 2) indexedVertex |> Array.toList
+
+
+sphVertex : Color.Color -> Float -> Float -> Float -> Attribute
+sphVertex color r phi theta =
+  Graphics.vertex
+    color
+    (r * sin theta * sin phi)
+    (r * cos theta)
+    (r * sin theta * cos phi)
+
+
+-- Geometric constants
+
+xAxis : Vec3.Vec3
+xAxis =
+  Vec3.vec3 1 0 0
+
+
+yAxis : Vec3.Vec3
+yAxis =
+  Vec3.vec3 0 1 0
+
+
+zAxis : Vec3.Vec3
+zAxis =
+  Vec3.vec3 0 0 1
