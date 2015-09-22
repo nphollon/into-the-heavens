6dbf5e8b71f1240454990b9ddf986c3ff38bf3a9
Nick Hollon <falling.masonry@gmail.com>
2015-09-22 16:26:44 -0700

    Move provisioning script; Set server name

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,6 +2,10 @@
 
 apt-get update
 apt-get install -y apache2
+
+echo "ServerName localhost" > /etc/apache2/conf-available/servername.conf
+a2enconf servername
 a2enmod userdir headers
+
 /usr/sbin/service apache2 restart
 
