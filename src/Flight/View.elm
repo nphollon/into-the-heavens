66881af41f9a5e60fe3f66ff9d8df077ff9336b9
Nick Hollon <falling.masonry@gmail.com>
2015-10-04 16:56:01 -0700

    Remove text from game. Expand webGL canvas 900x600

--- src/Flight/View.elm
+++ src/Flight/View.elm
@@ -17,50 +17,9 @@ import Infix exposing (..)
 
 view : Update.Data -> Layout.Element
 view model =
-  let
-    sceneWidth = 600
-    sceneHeight = 600
-    textBoxWidth = 300
-    textBoxHeight = 300
-    padding = 20
-    instructions =
-      String.join "\n"
-              [ "Turn the camera - w,a,s,d"
-              , "Roll the camera - q,e"
-              , "Fly forwards - i"
-              , "Fly backwards - m"
-              ]
-  in
-    Layout.flow Layout.right
-            [ fade sceneHeight
-            , scene sceneWidth sceneHeight model
-            , Layout.spacer padding 1
-            , Layout.flow Layout.down
-                      [ textBox textBoxWidth textBoxHeight model.message
-                      , Layout.spacer textBoxWidth 1
-                        |> Layout.color Palette.white
-                      , Layout.spacer 1 padding
-                      , textBox textBoxWidth textBoxHeight instructions
-                      ]
-            ]
+  scene 900 600 model
 
-fade height =
-  let
-    light x =
-      Layout.spacer x height
-
-    dark x =
-      Layout.color Palette.black (light x)
-  in
-    Layout.flow Layout.right
-          [ dark 2
-          , light 9
-          , dark 4
-          , light 4
-          , dark 9
-          , light 2
-          ]
-
+        
 scene : Int -> Int -> Update.Data -> Layout.Element
 scene width height model =
   let
@@ -83,12 +42,4 @@ scene width height model =
     WebGL.webgl (width, height) (background :: foreground)
       |> Layout.container width height Layout.middle
       |> Layout.color Palette.black
-              
-
-textBox : Int -> Int -> String -> Layout.Element
-textBox width height message =
-  Text.fromString message
-    |> Layout.leftAligned
-    |> Layout.size width height
-
 
