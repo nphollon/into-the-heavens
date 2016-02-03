7702aa0c4d8290e1742dc6f462dc124880cbb1a5
Nick Hollon <falling.masonry@gmail.com>
2016-02-02 16:21:49 -0800

    flesh out vector implementation

--- src/Math/Mechanics.elm
+++ src/Math/Mechanics.elm
@@ -5,41 +5,41 @@ import Math.Vector as Vector exposing (Vector)
 
 
 type alias State =
-  { time : Float
-  , particles : Dict String Particle
-  }
+    { time : Float
+    , particles : Dict String Particle
+    }
 
 
 type alias Particle =
-  { position : Vector
-  , velocity : Vector
-  , mass : Float
-  }
+    { position : Vector
+    , velocity : Vector
+    , mass : Float
+    }
 
 
 initialize : Dict String Particle -> State
 initialize dataDict =
-  { time = 0
-  , particles = dataDict
-  }
+    { time = 0
+    , particles = dataDict
+    }
 
 
 particle : String -> State -> Maybe Particle
 particle key state =
-  Dict.get key state.particles
+    Dict.get key state.particles
 
 
 totalMass : State -> Float
 totalMass =
-  .particles >> Dict.foldl (\_ p -> (+) p.mass) 0
+    .particles >> Dict.foldl (\_ p -> (+) p.mass) 0
 
 
 recenter : Particle -> Particle -> Particle
 recenter origin object =
-  { object
-    | position = Vector.subtract object.position origin.position
-    , velocity = Vector.subtract object.velocity origin.velocity
-  }
+    { object
+        | position = Vector.sub object.position origin.position
+        , velocity = Vector.sub object.velocity origin.velocity
+    }
 
 
 
@@ -48,73 +48,73 @@ recenter origin object =
 
 evolve : Rules -> Float -> State -> State
 evolve accel dt state =
-  let
-    a =
-      stateDerivative accel state
+    let
+        a =
+            stateDerivative accel state
 
-    b =
-      nudge (dt / 2) a state |> stateDerivative accel
+        b =
+            nudge (dt / 2) a state |> stateDerivative accel
 
-    c =
-      nudge (dt / 2) b state |> stateDerivative accel
+        c =
+            nudge (dt / 2) b state |> stateDerivative accel
 
-    d =
-      nudge dt c state |> stateDerivative accel
-  in
-    state
-      |> nudge (dt / 6) a
-      |> nudge (dt / 3) b
-      |> nudge (dt / 3) c
-      |> nudge (dt / 6) d
+        d =
+            nudge dt c state |> stateDerivative accel
+    in
+        state
+            |> nudge (dt / 6) a
+            |> nudge (dt / 3) b
+            |> nudge (dt / 3) c
+            |> nudge (dt / 6) d
 
 
 nudge : Float -> State -> State -> State
 nudge dt derivative state =
-  let
-    -- stateDerivative guarantees that the labels are the same
-    combine ( label, dpdt ) ( _, p ) =
-      (,)
-        label
-        { p
-          | position =
-              Vector.add p.position (Vector.scale dt dpdt.position)
-          , velocity =
-              Vector.add p.velocity (Vector.scale dt dpdt.velocity)
-        }
+    let
+        -- stateDerivative guarantees that the labels are the same
+        combine ( label, dpdt ) ( _, p ) =
+            (,)
+                label
+                { p
+                    | position =
+                        Vector.add p.position (Vector.scale dt dpdt.position)
+                    , velocity =
+                        Vector.add p.velocity (Vector.scale dt dpdt.velocity)
+                }
 
-    particles =
-      List.map2
-        combine
-        (Dict.toList derivative.particles)
-        (Dict.toList state.particles)
-        |> Dict.fromList
-  in
-    { state
-      | time = state.time + dt * derivative.time
-      , particles = particles
-    }
+        particles =
+            List.map2
+                combine
+                (Dict.toList derivative.particles)
+                (Dict.toList state.particles)
+                |> Dict.fromList
+    in
+        { state
+            | time = state.time + dt * derivative.time
+            , particles = particles
+        }
 
 
 stateDerivative : Rules -> State -> State
 stateDerivative accels state =
-  { state
-    | time = 1
-    , particles =
-        Dict.map
-          (\key particle ->
-            case Dict.get key accels of
-              Just accel ->
-                { particle
-                  | position = particle.velocity
-                  , velocity = accel particle state
-                }
+    { state
+        | time = 1
+        , particles =
+            Dict.map
+                (\key particle ->
+                    case Dict.get key accels of
+                        Just accel ->
+                            { particle
+                                | position = particle.velocity
+                                , velocity = accel particle state
+                            }
 
-              Nothing ->
-                particle
-          )
-          state.particles
-  }
+                        Nothing ->
+                            particle
+                )
+                state.particles
+    }
 
 
 type alias Rules =
-  Dict String (Particle -> State -> Vector)
+    Dict String (Particle -> State -> Vector)
--- src/Math/Vector.elm
+++ src/Math/Vector.elm
@@ -1,37 +1,100 @@
-module Math.Vector (vector, add, subtract, scale, Vector) where
+module Math.Vector (Vector, vector, getX, getY, getZ, add, sub, negate, scale, dot, cross, normalize, direction, length, lengthSquared, distance, distanceSquared) where
 
 
 type alias Vector =
-  { x : Float
-  , y : Float
-  , z : Float
-  }
+    { x : Float
+    , y : Float
+    , z : Float
+    }
 
 
 vector : Float -> Float -> Float -> Vector
 vector x y z =
-  { x = x, y = y, z = z }
+    { x = x, y = y, z = z }
+
+
+getX : Vector -> Float
+getX =
+    .x
+
+
+getY : Vector -> Float
+getY =
+    .y
+
+
+getZ : Vector -> Float
+getZ =
+    .z
 
 
 add : Vector -> Vector -> Vector
 add a b =
-  { x = a.x + b.x
-  , y = a.y + b.y
-  , z = a.z + b.z
-  }
+    { x = a.x + b.x
+    , y = a.y + b.y
+    , z = a.z + b.z
+    }
 
 
-subtract : Vector -> Vector -> Vector
-subtract a b =
-  { x = a.x - b.x
-  , y = a.y - b.y
-  , z = a.z - b.z
-  }
+sub : Vector -> Vector -> Vector
+sub a b =
+    { x = a.x - b.x
+    , y = a.y - b.y
+    , z = a.z - b.z
+    }
+
+
+negate : Vector -> Vector
+negate =
+    sub (vector 0 0 0)
 
 
 scale : Float -> Vector -> Vector
 scale c v =
-  { x = v.x * c
-  , y = v.y * c
-  , z = v.z * c
-  }
+    { x = v.x * c
+    , y = v.y * c
+    , z = v.z * c
+    }
+
+
+dot : Vector -> Vector -> Float
+dot u v =
+    u.x * v.x + u.y * v.y + u.z * v.z
+
+
+cross : Vector -> Vector -> Vector
+cross u v =
+    { x = u.y * v.z - u.z * v.y
+    , y = u.z * v.x - u.x * v.z
+    , z = u.x * v.y - u.y * v.x
+    }
+
+
+normalize : Vector -> Vector
+normalize v =
+    scale (1 / length v) v
+
+
+direction : Vector -> Vector -> Vector
+direction u v =
+    normalize (u `sub` v)
+
+
+length : Vector -> Float
+length =
+    lengthSquared >> sqrt
+
+
+lengthSquared : Vector -> Float
+lengthSquared v =
+    v `dot` v
+
+
+distance : Vector -> Vector -> Float
+distance u v =
+    length (u `sub` v)
+
+
+distanceSquared : Vector -> Vector -> Float
+distanceSquared u v =
+    lengthSquared (u `sub` v)
