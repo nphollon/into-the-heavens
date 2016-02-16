057fe86d091a1cb12285de5078fb008d424b0c5d
Nick Hollon <falling.masonry@gmail.com>
2016-02-16 10:19:09 -0800

    Collision unit tests

---
+++ test/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+temp_dir=${TMPDIR:-"/tmp"}
+elm_out="$temp_dir/test-raw.js"
+io_out="$temp_dir/test.js"
+io_sh="elm-stuff/packages/laszlopandy/elm-console/1.1.0/elm-io.sh"
+main_elm="Test.elm"
+
+cd test
+elm make $main_elm --output $elm_out --yes
+
+if [ $? -ne 0 ]; then exit 1; fi;
+
+bash $io_sh $elm_out $io_out
+if [ $? -ne 0 ]; then exit 1; fi;
+
+echo "Successfully generated $io_out"
+
+node $io_out
