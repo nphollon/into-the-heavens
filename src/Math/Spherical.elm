aea73ee145f1885fa70381990252846560c76b5a
Nick Hollon <falling.masonry@gmail.com>
2016-04-07 10:27:01 -0700

    Midpoint of great circle

--- src/Math/Spherical.elm
+++ src/Math/Spherical.elm
@@ -1,21 +1,41 @@
-module Math.Spherical (toRect, random) where
+module Math.Spherical (toRect, random, midpoint) where
 
 import Random.PCG as Random
 import Math.Vector as Vector exposing (Vector)
 
 
 toRect : Float -> Float -> Float -> Vector
-toRect radius latitude azimuth =
+toRect radius latitude longitude =
   Vector.vector
-    (radius * cos latitude * cos azimuth)
-    (radius * cos latitude * sin azimuth)
+    (radius * cos latitude * cos longitude)
+    (radius * cos latitude * sin longitude)
     (radius * sin latitude)
 
 
 random : (Float -> Float) -> Random.Generator Vector
 random f =
   Random.map3
-    (\r sinAlt az -> toRect (f r) (asin sinAlt) az)
+    (\r sinLat lon -> toRect (f r) (asin sinLat) lon)
     (Random.float 0 1)
     (Random.float -1 1)
     (Random.float 0 (turns 1))
+
+
+midpoint : ( Float, Float ) -> ( Float, Float ) -> ( Float, Float )
+midpoint ( latA, lonA ) ( latB, lonB ) =
+  let
+    bx =
+      (cos latB) * (cos (lonB - lonA))
+
+    by =
+      (cos latB) * (sin (lonB - lonA))
+
+    latMid =
+      atan2
+        (sin latA + sin latB)
+        (sqrt (by ^ 2 + (cos latA + bx) ^ 2))
+
+    lonMid =
+      lonA + (atan2 by (cos latA + bx))
+  in
+    ( latMid, lonMid )
--- test/Assertion.elm
+++ test/Assertion.elm
@@ -6,7 +6,7 @@ import Math.Vector as Vec exposing (Vector)
 
 assertEqualVector : Vector -> Vector -> Assertion
 assertEqualVector a b =
-  if Vec.distanceSquared a b < 1.0e-10 then
+  if isAboutZero (Vec.distanceSquared a b) then
     assert True
   else
     assertEqual a b
@@ -14,7 +14,23 @@ assertEqualVector a b =
 
 assertEqualFloat : Float -> Float -> Assertion
 assertEqualFloat a b =
-  if (a - b) ^ 2 < 1.0e-10 then
+  if isAboutZero ((a - b) ^ 2) then
     assert True
   else
     assertEqual a b
+
+
+assertEqualPair : ( Float, Float ) -> ( Float, Float ) -> Assertion
+assertEqualPair a b =
+  if
+    isAboutZero ((fst a - fst b) ^ 2)
+      && isAboutZero ((snd a - snd b) ^ 2)
+  then
+    assert True
+  else
+    assertEqual a b
+
+
+isAboutZero : Float -> Bool
+isAboutZero a =
+  a < 1.0e-10
