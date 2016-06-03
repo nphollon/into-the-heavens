a5e977613772033af3e12ca5840409d4144ebc6b
Nick Hollon <falling.masonry@gmail.com>
2016-06-02 17:28:00 -0700

    Reference local elm executable in make-mesh

--- make-mesh/make-mesh.sh
+++ make-mesh/make-mesh.sh
@@ -5,9 +5,10 @@ elm_out="$temp_dir/make-mesh-raw.js"
 io_out="$temp_dir/make-mesh.js"
 io_sh="elm-io.sh"
 main_elm="Main.elm"
+elm_make="node_modules/.bin/elm-make"
 
 cd make-mesh
-elm make $main_elm --output $elm_out --yes
+$elm_make $main_elm --output $elm_out --yes
 
 if [ $? -ne 0 ]; then exit 1; fi;
 
