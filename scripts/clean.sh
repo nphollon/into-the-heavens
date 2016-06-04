205ef38584f5bbecfbc1190d8591541b84be02ba
Nick Hollon <falling.masonry@gmail.com>
2016-06-03 17:01:11 -0700

    Add make-mesh artifacts to scripts/clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -8,7 +8,9 @@ if [[ $1 == "--full" ]];
 then
     rm -rf elm-stuff
     rm -rf test/elm-stuff
+    rm -rf make-mesh/elm-stuff
 else
     rm -rf elm-stuff/build-artifacts
     rm -rf test/elm-stuff/build-artifacts
+    rm -rf make-mesh/elm-stuff/build-artifacts
 fi;
