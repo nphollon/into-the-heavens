224b365271ee5771ec7f1b3b34f3cb758736a6bb
Nick Hollon <falling.masonry@gmail.com>
2016-07-05 10:34:12 -0700

    rename http-file-writer.js to file-server.js

--- make-mesh/make-mesh.sh
+++ make-mesh/make-mesh.sh
@@ -1,11 +1,8 @@
 #/usr/bin/env bash
 
-elm_out="make-mesh.html"
-file_writer="http-file-writer.js"
-
 cd make-mesh
-elm make Main.elm --yes --output $elm_out
+elm make Main.elm --yes --output "make-mesh.html"
 
 if [ $? -ne 0 ]; then exit 1; fi;
 
-node $file_writer
+node "file-server.js"
