eda8cf4be4dfce3eb0a3ab977840f8c5e99f7f65
Nick Hollon <falling.masonry@gmail.com>
2016-07-05 10:31:03 -0700

    http-file-writer responds to GET with file data
      Now POST writes to files in output directory, and GET reads files from
      input directory.
    
      Moved models/ into make-mesh

--- make-mesh/http-file-writer.js
+++ make-mesh/http-file-writer.js
@@ -1,39 +1,57 @@
-var http = require('http');
-var fs = require('fs');
+var http = require("http");
+var fs = require("fs");
 
 var port = 8090;
+var writeDir = "../public_html/data";
+var readDir = "models";
 
 var server = http.createServer(function (request, response) {
-    if (request.method === 'POST') {
-        var body = [];
-        var filename = "public_html/data" + request.url;
-        
-        request.on('data', function(chunk) {
-            body.push(chunk);
-        }).on('end', function() {
-            fs.writeFile(filename,
-                         Buffer.concat(body),
-                         { flag : "wx" },
-                         function (error) {
-                if (error) {
-                    console.log("Failed to write file " + filename);
-                    console.log(error);
-                    response.writeHead(500);
-                    response.end();
-                } else {
-                    response.writeHead(204);
-                    response.end();
-                }
-            });
-        });
+    if (request.method === "POST") {
+        writeFile(request, response);
     } else {
-        response.writeHead(404);
-        response.end();
+        readFile(request, response);
     }
 }).listen(port, function (error) {
     if (error) {
         console.log(error);
     } else {
-        console.log("Listening on port " + port.toString());
+        console.log("Listening on port " + port);
     }
 });
+
+var writeFile = function (request, response) {
+    var body = [];
+    var filename = writeDir + request.url;
+    
+    request.on("data", function(chunk) {
+        body.push(chunk);
+    }).on("end", function() {
+        fs.writeFile(filename, Buffer.concat(body), { flag : "wx" }, function (error) {
+            if (error) {
+                console.log("Failed to write file " + filename);
+                console.log(error);
+                response.writeHead(500);
+                response.end();
+            } else {
+                response.writeHead(204);
+                response.end();
+            }
+        });
+    });
+};
+
+var readFile = function (request, response) {
+    var filename = readDir + request.url;
+    fs.readFile(filename, function (error, text) {
+        if (error) {
+            console.log("Failed to read file " + filename);
+            console.log(error);
+            response.writeHead(404);
+            response.end();
+        } else {
+            response.writeHead(200);
+            response.write(text);
+            response.end();
+        }
+    });
+};
